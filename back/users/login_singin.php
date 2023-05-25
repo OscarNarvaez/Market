6dc1f190db82f239121d5bd2c523a37e1d946a01
unknown <julinnarget@gmail.com>
2023-05-25 11:13:47 -0500

    Hoja de estilos para la apariencia en create_users.html y login.html

--- back/users/login_singin.php
+++ back/users/login_singin.php
@@ -5,7 +5,8 @@ $password = $_POST["password"];
 $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    header("refresh:0; url=http://localhost/market/front/index.html");
+    echo "<script> alert ('¡Welcome!')</script>";
+    header("refresh:0; url=http://127.0.0.1:5500/index.html");
 } else {
     echo "<script> alert ('¡User does not exist!')</script>";
     header("refresh:0; url=http://localhost/market/front/login.html");
